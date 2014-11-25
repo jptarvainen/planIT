a723c520ceccd7aa121f90b736f3de595e6edf4c
Juha-Pekka Tarvainen <juhapekka.tarvainen@gmail.com>
2014-11-25 21:30:26 +0200

    initilized rails projects

---
+++ planit/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Planit::Application.config.secret_token = '2315a4b81cac9c2cc67b77823711ac3e79584b8eb21d2f98e04191c8a280691ebb42ffcb7a5ceb6d15fc2f0d205046de0791bf33bc6df78e99af9bf378359ec1'
